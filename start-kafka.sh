efc4c2356ac8a399b16722e0473155cfa061dabb
Benoit Delbosc <bdelbosc@nuxeo.com>
2017-06-30 17:46:59 +0200

    Update to use Kakfka 0.11.0.0 with group.initial.rebalance.delay.ms of 10ms

--- start-kafka.sh
+++ start-kafka.sh
@@ -40,6 +40,7 @@ do
     if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
         sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
     else
+	echo "" >> $KAFKA_HOME/config/server.properties
         echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
     fi
   fi
